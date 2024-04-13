6050a8dd0a7cec5e534e50d8f4a91ee5e0ae8263
Janki Prajapati <166908235+jankiprajapati@users.noreply.github.com>
2024-04-13 03:16:53 -1200

    Create sg.tf

---
+++ terraform-script/sg.tf
@@ -0,0 +1,74 @@
+#security groups
+#To Create Security Group
+resource "aws_security_group" "sg1" {
+  name        = "ssh-http"
+  description = "Allow SSH and HTTP from all-inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+   description      = "allow all traffic"
+   from_port        = 0
+   to_port          = 0
+   protocol         = "-1"
+   cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "sg2" {
+  name        = "ssh"
+  description = "Allow SSH from all-inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+   description      = "allow all traffic"
+   from_port        = 0
+   to_port          = 0
+   protocol         = "-1"
+   cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "http"
+  description = "Allow HTTP from all-inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+   description      = "allow all traffic"
+   from_port        = 0
+   to_port          = 0
+   protocol         = "-1"
+   cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
