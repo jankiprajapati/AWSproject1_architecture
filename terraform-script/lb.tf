44b01a44e6e76aafc8db249974985e924734c906
Janki Prajapati <166908235+jankiprajapati@users.noreply.github.com>
2024-04-13 03:12:39 -1200

    Create lb.tf

---
+++ terraform-script/lb.tf
@@ -0,0 +1,20 @@
+resource "aws_lb" "loadbalancer" {
+  name               = "${var.name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.targetgroup.arn
+  }
+}
